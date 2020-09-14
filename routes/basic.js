265d470c66f48cd30472394317e5664061654449
bengben <benben@hotmail.com>
2020-09-15 07:17:36 +0800

    完成抓取单个项目基本信息

--- routes/basic.js
+++ routes/basic.js
@@ -22,18 +22,25 @@ router.get("/initProjects", function (req, res, next) {
  * 初始化项目信息
  */
 router.get("/updateProjects", function (req, res, next) {
-   spider.updateProjects(res);
- });
-
-
- /**
- * 得到项目详细信息
- */
-router.get("/initPackage", function (req, res, next) {
-  res.send(spider.getInfoById(879767969620824064));
+  spider.updateProjects(res);
 });
 
- 
+
+/**
+ * 得到项目详细信息
+ */
+router.get("/initPackage", async function (req, res, next) {
+  console.log('/initPackage');
+  // spider.initProjectDetail(res);
+  // res.send(result);
+  spider.getInfoById();
+  // var a = await spider.getInfoById();
+  // var b = await spider.getAttchments();
+  // console.log('aaaaaa:', a);
+  // console.log('bbbbbb:', b);
+});
+
+
 
 
 
